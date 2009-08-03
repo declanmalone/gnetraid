a8d057e6112f95b693da2955289318730cc2cac3
Ida Black <idablack@users.sourceforge.net>
2009-08-03 15:52:42 +0000

    New Matrix-related code
    git-svn-id: https://svn.code.sf.net/p/gnetraid/code@25 e6afed3b-5b75-4bb5-bfa7-be4a7fb757eb

---
+++ Math-FastGF2/trunk/lib/Math/FastGF2/Matrix.pm
@@ -0,0 +1,533 @@
+
+package Math::FastGF2::Matrix;
+
+use 5.008000;
+use strict;
+use warnings;
+use Carp;
+
+use Math::FastGF2 ":ops";
+
+use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
+
+require Exporter;
+
+@ISA = qw(Exporter Math::FastGF2);
+%EXPORT_TAGS = ( 'all' => [ qw( ) ],
+	       );
+@EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+@EXPORT = (  );
+$VERSION = '0.01';
+
+require XSLoader;
+XSLoader::load('Math::FastGF2', $VERSION);
+
+our @orgs=("undefined", "rowwise", "colwise");
+
+sub new {
+  my $proto  = shift;
+  my $class  = ref($proto) || $proto;
+  my $parent = ref($proto) && $proto;
+  my %o=
+    (
+     rows => undef,
+     cols => undef,
+     width => undef,
+     org => undef,
+     @_,
+    );
+  my $org;			# numeric value 1==ROWWISE, 2==COLWISE
+  my $errors=0;
+
+  foreach (qw(rows cols width)) {
+    unless (defined($o{$_})) {
+      carp "required parameter '$_' not supplied";
+      ++$errors;
+    }
+  }
+
+  if (defined($o{"org"})) {
+    if ($o{"org"} eq "rowwise") {
+      #carp "setting org to 1 as requested";
+      $org=1;
+    } elsif ($o{"org"} eq "colwise") {
+      #carp "setting org to 2 as requested";
+      $org=2;
+    } else {
+      carp "value of 'org' parameter should be 'rowwise' or 'colwise'";
+      ++$errors;
+    }
+  } else {
+    #carp "defaulting org to 1";
+    $org=1;			# default to ROWWISE
+  }
+
+  if ($o{width} != 1 and $o{width} !=2 and $o{width} != 4) {
+    carp "Invalid width $o{width} (must be 1, 2 or 4)";
+    ++$errors;
+  }
+
+  return undef if $errors;
+
+  return alloc_c($class,$o{rows},$o{cols},$o{width},$org);
+
+}
+
+sub new_identity {
+  my $proto  = shift;
+  my $class  = ref($proto) || $proto;
+  my $parent = ref($proto) && $proto;
+  my %o = (
+	   size  => undef,
+	   org   => "rowwise",	# default to rowwise
+	   width => undef,
+	   @_
+	  );
+  unless (defined($o{size}) and $o{size} > 0) {
+    carp "new_identity needs a size argument";
+    return undef;
+  }
+  unless (defined($o{width}) and ($o{width}==1 or $o{width}==2 or
+				  $o{width}==4)) {
+    carp "new_identity needs width parameter of 1, 2 or 4";
+    return undef;
+  }
+  unless (defined($o{org}) and ($o{org} eq "rowwise"
+				or $o{org}== "colwise")) {
+    carp "new_identity org parameter must be 'rowwise' or 'colwise'";
+    return undef;
+  }
+  my $org = ($o{org} eq "rowwise" ? 1 : 2);
+
+  my $id=alloc_c($class,$o{size},$o{size},$o{width},$org);
+  return undef unless $id;
+  for my $i (0 .. $o{size} - 1 ) {
+    $id->setval($i,$i,1);
+  }
+  return $id;
+}
+
+sub ORG {
+  my $self=shift;
+  #carp "Numeric organisation value is " . $self->ORGNUM;
+  return $orgs[$self->ORGNUM];
+}
+
+sub multiply {
+  my $self   = shift;
+  my $class  = ref($self);
+  my $other  = shift;
+  my $result = shift;
+
+  unless (defined($other) and ref($other) eq $class) {
+    carp "need another matrix to multiply by";
+    return undef;
+  }
+  unless ($self->COLS == $other->ROWS) {
+    carp "this matrix's COLS must equal other's ROWS";
+    return undef;
+  }
+  unless ($self->WIDTH == $other->WIDTH) {
+    carp "can only multiply two matrices with the same WIDTH";
+    return undef;
+  }
+
+  if (defined($result)) {
+    unless (ref($result) eq $class) {
+      carp "result object is not a matrix";
+      return undef;
+    }
+    unless ($self->ROWS == $result->ROWS) {
+      carp "this matrix's ROWS must equal result's ROWS";
+      return undef;
+    }
+    unless ($self->WIDTH == $result->WIDTH) {
+      carp "result matrix's WIDTH does not match this ones.";
+      return undef;
+    }
+  } else {
+    $result=new($class, rows=>$self->ROWS, cols =>$other->COLS,
+			width=> $self->WIDTH, org=>$self->ORG);
+    unless (defined ($result) and ref($result) eq $class) {
+      carp "Problem allocating new RESULT matrix";
+      return undef;
+    }
+  }
+
+  multiply_submatrix_c($self, $other, $result,
+		       0,0,$self->ROWS,
+		       0,0,$other->COLS);
+  return $result;
+}
+
+sub eq {
+  my $self   = shift;
+  my $class  = ref($self);
+  my $other  = shift;
+
+  unless (defined($other) and ref($other) eq $class) {
+    carp "eq needs another matrix to compare against";
+    return undef;
+  }
+  unless ($self->COLS == $other->COLS) {
+    return 0;
+  }
+  unless ($self->COLS == $other->COLS) {
+    return 0;
+  }
+  unless ($self->WIDTH == $other->WIDTH) {
+    return 0;
+  }
+  return values_eq_c($self,$other);
+}
+
+
+sub ne {
+  my $self   = shift;
+  my $class  = ref($self);
+  my $other  = shift;
+
+  unless (defined($other) and ref($other) eq $class) {
+    carp "eq needs another matrix to compare against";
+    return undef;
+  }
+  if ($self->COLS != $other->COLS) {
+    return 1;
+  }
+  if ($self->COLS != $other->COLS) {
+    return 1;
+  }
+  if ($self->WIDTH != $other->WIDTH) {
+    return 1;
+  }
+  return !values_eq_c($self,$other);
+}
+
+sub offset_to_rowcol {
+  my $self=shift;
+  my $offset=shift;
+
+  if ($offset % $self->WIDTH) {
+    carp "offset must be a multiple of WIDTH in offset_to_rowcol";
+    return undef;
+  }
+  $offset /= $self->WIDTH;
+  if ($offset < 0 or $offset >= $self->ROWS * $self->COLS) {
+    carp "Offset out of range in offset_to_rowcol";
+    return undef;
+  }
+
+  if ($self->ORG eq "rowwise") {
+    return ((int ($offset / $self->COLS)),
+	    ($offset % $self->COLS) );
+  } else {
+    return (($offset % $self->ROWS),
+	    (int ($offset / $self->ROWS)));
+  }
+}
+
+sub rowcol_to_offset {
+  my $self=shift;
+  my $row=shift;
+  my $col=shift;
+
+  if ($row < 0 or $row >= $self->ROWS) {
+    carp "ROW out of range in rowcol_to_offset";
+    return undef;
+  }
+  if ($col < 0 or $col >= $self->COLS) {
+    carp "COL out of range in rowcol_to_offset";
+    return undef;
+  }
+  if ($self->ORG eq "rowwise") {
+    return ($row * $self->COLS + $col) / $self->WIDTH;
+  } else {
+    return ($col * $self->ROWS + $row) / $self->WIDTH;
+  }
+}
+
+sub getvals {
+  my $self  = shift;
+  my $class = ref($self);
+  my $row   = shift;
+  my $col   = shift;
+  my $bytes = shift;
+  my $order = shift || 0;
+  my $want_list = wantarray;
+
+  #carp "Asked to read ROW=$row, COL=$col, len=$bytes (bytes)\n";
+
+  unless ($class) {
+    carp "getvals only operates on an object instance";
+    return undef;
+  }
+  if ($bytes % $self->WIDTH) {
+    carp "bytes to get must be a multiple of WIDTH";
+    return undef;
+  }
+  unless (defined($row) and defined($col) and defined($bytes)) {
+    carp "getvals requires row, col, words parameters";
+    return undef;
+  }
+  if ($order < 0 or $order > 2) {
+    carp "order ($order) != 0 (native), 1 (little-endian) or 2 (big-endian)";
+    return undef;
+  }
+  my $width=$self->WIDTH;
+  my $msize=$self->ROWS * $self->COLS;
+  if ($row < 0 or $row >= $self->ROWS) {
+    carp "starting row out of range";
+    return undef;
+  }
+  if ($col < 0 or $row >= $self->ROWS) {
+    carp "starting row out of range";
+    return undef;
+  }
+
+  my $s=get_raw_values_c($self, $row, $col, $bytes * $width, $order);
+
+  return $s unless $want_list;
+
+  # Since the get_raw_values_c call swaps byte order, we don't do it here
+  if ($self->WIDTH == 1) {
+    return unpack "C*", $s;
+  } elsif ($self->WIDTH == 2) {
+    return unpack "S*", $s
+  } else {
+    return unpack "L*", $s;
+  }
+
+  # return unpack ($self->WIDTH == 2 ? "v*" : "V*"), $s;
+  # return unpack ($self->WIDTH == 2 ? "n*" : "N*"), $s;
+}
+
+sub setvals {
+  my $self    = shift;
+  my $class   = ref($self);
+  my ($row, $col, $vals, $order) = @_;
+  my ($str,$words);
+  $order=0 unless defined($order);
+
+  #carp "Asked to write ROW=$row, COL=$col, len=" . length($vals) . "\n";
+
+  unless ($class) {
+    carp "getvals only operates on an object instance";
+    return undef;
+  }
+  unless (defined($row) and defined($col)) {
+    carp "getvals requires row, col, order parameters";
+    return undef;
+  }
+  if ($order < 0 or $order > 2) {
+    carp "order != 0 (native), 1 (little-endian) or 2 (big-endian)";
+    return undef;
+  }
+  if ($row < 0 or $row >= $self->ROWS) {
+    carp "starting row out of range";
+    return undef;
+  }
+  if ($col < 0 or $row >= $self->ROWS) {
+    carp "starting row out of range";
+    return undef;
+  }
+
+  if(ref($vals)) {
+    # treat $vals as a list(ref) of numbers
+    unless ($words=scalar(@$vals)) {
+      carp "setvals: values must be either a string or reference to a list";
+      return undef;
+    }
+    if ($self->WIDTH == 1) {
+      $str=pack "C*", @$vals;
+    } elsif ($self->WIDTH == 2) {
+      $str=pack "S*", @$vals;
+    } else {
+      $str=pack "L*", @$vals;
+    }
+  } else {
+    # treat vals as a string
+    $str="$vals";
+    $words=(length $str) / $self->WIDTH;
+  }
+
+  my $msize=$self->ROWS * $self->COLS;
+  if ( (($self->ORG eq "rowwise") and
+	($words + $self->COLS * $row + $col > $msize)) or
+       ($words + $self->ROWS * $col + $row > $msize)) {
+    carp "string length exceeds matrix size";
+    return undef;
+  }
+
+  set_raw_values_c($self, $row, $col, $words, $order, $str);
+  return $str;
+}
+
+# return new matrix with self on left, other on right
+sub concat {
+  my $self  = shift;
+  my $class = ref($self);
+  my $other = shift;
+
+  unless (defined($other) and ref($other) eq $class) {
+    carp "concat needs a second matrix to operate on";
+    return undef;
+  }
+  unless ($self->WIDTH == $other->WIDTH) {
+    carp "concat: incompatible matrix widths";
+    return undef;
+  }
+  unless ($self->ROWS == $other->ROWS) {
+    carp "can't concat: the matrices have different number of rows";
+    return undef;
+  }
+
+  my $cat=alloc_c($class, $self->ROWS, $self->COLS + $other->COLS,
+		  $self->WIDTH, $self->ORGNUM);
+  return undef unless defined $cat;
+  if ($self->ORG eq "rowwise") {
+    my $s;
+    for my $row (0.. $other->ROWS - 1) {
+      $s=get_raw_values_c($self, $row, 0, $self->COLS, 0);
+      set_raw_values_c   ($cat,  $row, 0, $self->COLS, 0, $s);
+      for my $col (0.. $other->COLS - 1) {
+	$cat->setval($row, $self->COLS + $col,
+		     $other->getval($row,$col));
+      }
+    }
+  } else {
+    my $s;
+    $s=get_raw_values_c($self, 0, 0, $self->COLS * $self->ROWS, 0);
+    set_raw_values_c   ($cat,  0, 0, $self->COLS * $self->ROWS, 0, $s);
+    for my $row (0.. $other->ROWS - 1) {
+      for my $col (0.. $other->COLS - 1) {
+	$cat->setval($row, $self->COLS + $col,
+		     $other->getval($row,$col));
+      }
+    }
+  }
+
+  return $cat;
+}
+
+# I'll replace this with some C code later
+sub solve {
+
+  my $self  = shift;
+  my $class = ref($self);
+
+  my $rows=$self->ROWS;
+  my $cols=$self->COLS;
+  my $order=$self->WIDTH * 8;
+
+  unless ($cols > $rows) {
+    carp "solve only works on matrices with COLS > ROWS";
+    return undef;
+  }
+
+  local *swap_rows = sub {
+    my ($row1, $row2, $start_col) = @_;
+    return if $row1==$row2;
+
+    my ($s,$t,$col);
+    if ($self->ORG eq "rowwise") {
+      $s=get_raw_values_c($self, $row1, $start_col, $self->COLS - $start_col, 0);
+      $t=get_raw_values_c($self, $row2, $start_col, $self->COLS - $start_col, 0);
+      set_raw_values_c   ($self, $row1, $start_col, $self->COLS - $start_col, 0, $t);
+      set_raw_values_c   ($self, $row2, $start_col, $self->COLS - $start_col, 0, $s);
+    } else {
+      for $col ($start_col .. $cols -1) {
+	$s=$self->getval($row1,$col);
+	$t=$self->getval($row2,$col);
+	$self->setval($row1, $col, $t);
+	$self->setval($row2, $col, $s);
+      }
+    }
+  };
+
+  # work down the diagonal one row at a time ...
+  for my $row (0 .. $rows - 1) {
+
+    # We have to check whether the matrix is non-singular; all k x k
+    # sub-matrices generated by the split part of the IDA are
+    # guaranteed to be invertible, but user-supplied matrices may not
+    # be, so we have to test for this.
+
+    if ($self->getval($row,$row) == 0) {
+      print "had to swap zeros\n";
+      my $found=undef;
+      for my $other_row ($row + 1 .. $rows - 1) {
+	next if $row == $other_row;
+	if ($self->getval($other_row,$row) != 0) {
+	  $found=$other_row;
+	  last;
+	}
+      }
+      return undef unless defined $found;
+      swap_rows($row,$found,$row);
+    }
+
+    # normalise the current row first
+    my $diag_inverse = gf2_inv($order,$self->getval($row,$row));
+
+    $self->setval($row,$row,1);
+    for my $col ($row + 1 .. $cols - 1) {
+      $self->setval($row,$col,
+	gf2_mul($order, $self->getval($row,$col), $diag_inverse));
+    }
+
+    # zero all elements above and below ...
+    for my $other_row (0 .. $rows - 1) {
+      next if $row == $other_row;
+
+      my $other=$self->getval($other_row,$row);
+      next if $other == 0;
+      $self->setval($other_row,$row,0);
+      for my $col ($row + 1 .. $cols - 1) {
+	$self->setval($other_row,$col,
+	  gf2_mul($order, $self->getval($row,$col), $other) ^
+	    $self->getval($other_row,$col));
+      }
+    }
+  }
+
+  my $result=alloc_c($class, $rows, $cols - $rows,
+		     $self->WIDTH, $self->ORGNUM);
+  for my $row (0 .. $rows - 1) {
+    for my $col (0 .. $cols - $rows - 1) {
+      $result->setval($row,$col,
+		      $self->getval($row, $col + $rows));
+    }
+  }
+
+  return $result;
+}
+
+sub invert {
+
+  my $self  = shift;
+  my $class = ref($self);
+
+  unless ($self->COLS == $self->ROWS) {
+    carp "invert only works on square matrices";
+    return undef;
+  }
+
+  my $cat=$self->concat($self->new_identity(size => $self->COLS,
+					    width => $self->WIDTH));
+
+  return undef unless defined ($cat);
+
+  return $cat->solve;
+}
+
+1;
+
+
+=head1 NAME
+
+Math::FastGF2::Matrix - Matrix operations for fast Galois Field arithmetic
+
+=cut
+
+
+
---
+++ Math-FastGF2/trunk/t/Matrix.t
@@ -0,0 +1,226 @@
+# -*- Perl -*-
+
+use Test::More tests => 67;
+BEGIN { use_ok('Math::FastGF2::Matrix', ':all') };
+
+my $m=Math::FastGF2::Matrix->new(rows=>2, cols =>2, width=>1);
+my $failed;
+my $class="Math::FastGF2::Matrix";
+
+ok(ref($m) eq $class,    "new returns correct class?");
+ok($m->ROWS == 2,        "ROWS returns correct value?");
+ok($m->COLS == 2,        "COLS returns correct value?");
+ok($m->WIDTH == 1,       "WIDTH returns correct value?");
+ok($m->ORG eq "rowwise", "ORG returns correct value?");
+
+$failed=0;
+map { ++$failed if $m->getval($_/2, $_ & 1) } 0..3;
+ok (!$failed,  "All values initialised to zero?");
+
+$failed=0;
+map { ++$failed if $_ * 7 + 1 != $m->setval($_/2, $_ & 1, $_ * 7 + 1) } 0..3;
+ok (!$failed,  "setval returns set value?");
+
+$failed=0;
+map { ++$failed if $_ * 7 + 1 != $m->getval($_/2, $_ & 1) } 0..3;
+ok (!$failed,  "setval/getval returns same value?");
+
+my @mat8x8 = (
+	      ["35","36","82","7A","D2","7D","75","31"],
+	      ["0E","76","C3","B0","97","A8","47","14"],
+	      ["F4","42","A2","7E","1C","4A","C6","99"],
+	      ["3D","C6","1A","05","30","B6","42","0F"],
+	      ["81","6E","F2","72","4E","BC","38","8D"],
+	      ["5C","E5","5F","A5","E4","32","F8","44"],
+	      ["89","28","94","3C","4F","EC","AA","D6"],
+	      ["54","4B","29","B8","D5","A4","0B","2C"],
+	     );
+my @inv8x8= (
+	     ["3E","02","23","87","8C","C0","4C","79"],
+	     ["5D","2B","2A","5B","7E","FE","25","36"],
+	     ["F2","A9","B5","57","A2","F6","A2","7D"],
+	     ["11","5E","E4","61","59","F4","B9","42"],
+	     ["D5","16","B8","5B","30","85","1E","72"],
+	     ["3B","F7","1B","5B","4C","55","35","04"],
+	     ["58","95","73","33","8A","77","1C","F4"],
+	     ["59","C0","7B","13","9F","8B","BE","E3"],
+	    );
+my @identity8x8= (
+	    [1,0,0,0,0,0,0,0],
+	    [0,1,0,0,0,0,0,0],
+	    [0,0,1,0,0,0,0,0],
+	    [0,0,0,1,0,0,0,0],
+	    [0,0,0,0,1,0,0,0],
+	    [0,0,0,0,0,1,0,0],
+	    [0,0,0,0,0,0,1,0],
+	    [0,0,0,0,0,0,0,1],
+	   );
+
+# in-place conversion of hex strings to decimal values
+map { map { $_ = hex } @$_ } @mat8x8;
+map { map { $_ = hex } @$_ } @inv8x8;
+
+my ($r,$c,$m8x8,$i8x8,$r8x8,$id8x8);
+$m8x8 = Math::FastGF2::Matrix->new(rows=>8, cols =>8, width=>1);
+$i8x8 = Math::FastGF2::Matrix->new(rows=>8, cols =>8, width=>1);
+$id8x8= Math::FastGF2::Matrix->new(rows=>8, cols =>8, width=>1);
+
+ok(ref($m8x8)  eq "Math::FastGF2::Matrix", "Create 8x8 matrix OK?");
+ok(ref($i8x8)  eq "Math::FastGF2::Matrix", "Create 8x8 inverse matrix OK?");
+ok(ref($id8x8) eq "Math::FastGF2::Matrix", "Create 8x8 result matrix OK?");
+
+for $r (0..7) {
+  for $c (0..7) {
+    $m8x8 ->setval($r,$c,$mat8x8[$r][$c]);
+    $i8x8 ->setval($r,$c,$inv8x8[$r][$c]);
+    $id8x8->setval($r,$c,$identity8x8[$r][$c]);
+  }
+}
+
+# multiply without supplying a result matrix
+$r8x8=$m8x8->multiply($i8x8);
+ok(defined($r8x8),   "multiply method returns some value?");
+ok(ref($r8x8) eq "Math::FastGF2::Matrix", "multiply returns correct class?");
+
+# multiply with a supplied result matrix
+my $r2=$m8x8->multiply($i8x8,$r8x8);
+ok(defined($r2),  "multiply returns value with supplied result matrix?");
+ok($r2 eq $r8x8,  "multiply returns supplied result matrix?");
+
+# Checking equality
+ok($r8x8->eq($r2),   "Equality test on same matrix?");
+ok($r8x8->ne($m),    "Inequality test on differently-sized matrices?");
+ok($r8x8->ne($m8x8), "Inequality test on differently-valued matrices?");
+
+# is matrix x inverse = identity?
+ok($r8x8->eq($id8x8), "Matrix x Inverse == Identity?");
+
+# Test getvals in scalar context
+my $row="3536827AD27D7531";	  # first row of m8x8
+my $got= $m8x8->getvals(0,0,8); # get first 8 values
+my $packed_row=pack "H16", $row;
+ok (length $got == 8, "Scalar return from getvals of correct length?");
+ok ($got eq $packed_row,  "Correct scalar return from getvals?");
+
+# Test getvals in list context
+my @row=$m8x8->getvals(0,0,8); # get first 8 values
+ok (scalar(@row) == 8,  "number of returned items in getvals list?");
+$failed=0;
+map { ++$failed if $row[$_] != $m8x8->getval(0, $_) } 0..7;
+ok (!$failed,  "Correct list return from getvals?");
+
+# Test setvals... using ROWWISE
+$m=Math::FastGF2::Matrix->new(rows => 2, cols => 2, width => 1,
+			      org=> "rowwise"); # be explicit
+my @vals=(65, 66, 67, 68);
+my $str=$m->setvals(0,0,\@vals);
+ok (length $str == 4, "setvals returns string of correct length, given list?");
+
+my $str2=$m->getvals(0,0,4);
+
+ok ($str eq $str2, "getvals ($str2) == setvals ($str)?");
+
+ok ($m->getval(0,0) == $vals[0],  "setvals (rowwise) sets (0,0) correctly?");
+ok ($m->getval(0,1) == $vals[1],  "setvals (rowwise) sets (0,1) correctly?");
+ok ($m->getval(1,0) == $vals[2],  "setvals (rowwise) sets (1,0) correctly?");
+ok ($m->getval(1,1) == $vals[3],  "setvals (rowwise) sets (1,1) correctly?");
+
+# Test setvals... using COLWISE
+$m=Math::FastGF2::Matrix->new(rows => 2, cols => 2, width => 1,
+			      org=> "colwise"); # be explicit
+ok (ref($m) eq $class,  "Created colwise matrix OK?");
+$str=$m->setvals(0,0,\@vals);
+ok (length $str == 4, "setvals returns string of correct length, given list?");
+
+$str2=$m->getvals(0,0,4);
+
+ok ($str eq $str2, "getvals ($str2) == setvals ($str)?");
+
+ok ($m->getval(0,0) == $vals[0],  "setvals ([], colwise) sets (0,0) correctly?");
+ok ($m->getval(1,0) == $vals[1],  "setvals ([], colwise) sets (1,0) correctly?");
+ok ($m->getval(0,1) == $vals[2],  "setvals ([], colwise) sets (0,1) correctly?");
+ok ($m->getval(1,1) == $vals[3],  "setvals ([], colwise) sets (1,1) correctly?");
+
+# setvals with a string..
+$m=Math::FastGF2::Matrix->new(rows => 2, cols => 2, width => 1,
+			      org=> "colwise");
+$str=$m->setvals(0,0,$str);
+ok ($m->getval(0,0) == $vals[0],  "setvals (\$\$, colwise) sets (0,0) correctly?");
+ok ($m->getval(1,0) == $vals[1],  "setvals (\$\$, colwise) sets (1,0) correctly?");
+ok ($m->getval(0,1) == $vals[2],  "setvals (\$\$, colwise) sets (0,1) correctly?");
+ok ($m->getval(1,1) == $vals[3],  "setvals (\$\$, colwise) sets (1,1) correctly?");
+
+# Checking equality #2... rowwise vs colwise matrix
+my @by_row=(65,66,67,68);
+my @by_col=(65,67,66,68);
+my $m_by_row=Math::FastGF2::Matrix->new(rows => 2, cols => 2, width => 1,
+					 org=> "rowwise");
+my $m_by_col=Math::FastGF2::Matrix->new(rows => 2, cols => 2, width => 1,
+					 org=> "colwise");
+$m_by_row->setvals(0,0,\@by_row);
+$m_by_col->setvals(0,0,\@by_col);
+
+ok ($m_by_row->eq($m_by_col),        "rowwise-to-colwise compare eq?");
+$m_by_col->setval(1,1,69);
+ok ($m_by_row->ne($m_by_col),        "rowwise-to-colwise compare ne?");
+
+# Check rowcol_to_offset and offset_to_rowcol
+my ($offset);
+
+ok (((($r,$c)=$m_by_col->offset_to_rowcol(0)),
+    $r==0 and $c==0),      "Offset 0 -> (0,0) (colwise)?");
+ok (((($r,$c)=$m_by_col->offset_to_rowcol(1)),
+     $r==1 and $c==0),      "Offset 1 -> (1,0) (colwise)?");
+ok (((($r,$c)=$m_by_col->offset_to_rowcol(2)),
+    $r==0 and $c==1),      "Offset 2 -> (0,1) (colwise)?");
+ok (((($r,$c)=$m_by_col->offset_to_rowcol(3)),
+     $r==1 and $c==1),      "Offset 3 -> (1,1) (colwise)?");
+
+ok (((($r,$c)=$m_by_row->offset_to_rowcol(0)),
+     $r==0 and $c==0),      "Offset 0 -> (0,0) (rowwise)?");
+ok (((($r,$c)=$m_by_row->offset_to_rowcol(1)),
+     $r==0 and $c==1),      "Offset 1 -> (0,1) (rowwise)?");
+ok (((($r,$c)=$m_by_row->offset_to_rowcol(2)),
+     $r==1 and $c==0),      "Offset 2 -> (1,0) (rowwise)?");
+ok (((($r,$c)=$m_by_row->offset_to_rowcol(3)),
+     $r==1 and $c==1),      "Offset 3 -> (1,1) (rowwise)?");
+
+ok ($m_by_row->rowcol_to_offset(0,0) == 0,
+                              "(0,0) -> Offset 0  (rowwise)?");
+ok ($m_by_row->rowcol_to_offset(0,1) == 1,
+                              "(0,1) -> Offset 1  (rowwise)?");
+ok ($m_by_row->rowcol_to_offset(1,0) == 2,
+                              "(1,0) -> Offset 2  (rowwise)?");
+ok ($m_by_row->rowcol_to_offset(1,1) == 3,
+                              "(1,1) -> Offset 3  (rowwise)?");
+
+ok ($m_by_col->rowcol_to_offset(0,0) == 0,
+                              "(0,0) -> Offset 0  (colwise)?");
+ok ($m_by_col->rowcol_to_offset(0,1) == 2,
+                              "(0,1) -> Offset 2  (colwise)?");
+ok ($m_by_col->rowcol_to_offset(1,0) == 1,
+                              "(1,0) -> Offset 1  (colwise)?");
+ok ($m_by_col->rowcol_to_offset(1,1) == 3,
+                              "(1,1) -> Offset 3  (colwise)?");
+
+# Test above using non-square matrices
+$m=Math::FastGF2::Matrix->new(rows => 3, cols => 7, width => 1,
+			      org=> "colwise");
+ok ($m->rowcol_to_offset(0,0) == 0,
+                              "(0,0) -> Offset 0  (colwise)?");
+ok ($m->rowcol_to_offset(0,6) == 18,
+                              "(0,6) -> Offset 18  (colwise)?");
+ok ($m->rowcol_to_offset(2,0) == 2,
+                              "(2,0) -> Offset 2  (colwise)?");
+ok ($m->rowcol_to_offset(2,6) == 20,
+                              "(2,6) -> Offset 20  (colwise)?");
+
+ok (((($r,$c)=$m->offset_to_rowcol(0)),
+    $r==0 and $c==0),      "Offset 0 -> (0,0) (colwise)?");
+ok (((($r,$c)=$m->offset_to_rowcol(2)),
+     $r==2 and $c==0),     "Offset 2 -> (2,0) (colwise)?");
+ok (((($r,$c)=$m->offset_to_rowcol(18)),
+    $r==0 and $c==6),      "Offset 18 -> (0,6) (colwise)?");
+ok (((($r,$c)=$m->offset_to_rowcol(20)),
+     $r==2 and $c==6),     "Offset 20 -> (2,6) (colwise)?");
+
