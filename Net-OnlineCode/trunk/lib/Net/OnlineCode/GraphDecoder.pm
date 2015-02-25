dd02c2a9ab2070f4e2a42835d3e1568ed97ae71c
Declan Malone <idablack@users.sourceforge.net>
2015-02-25 22:16:31 +0000

    Fix bug where check block wasn't adding reciprocal links. Go back to old way of doing up-propagation (using aux_rule).

--- Net-OnlineCode/trunk/lib/Net/OnlineCode/GraphDecoder.pm
+++ Net-OnlineCode/trunk/lib/Net/OnlineCode/GraphDecoder.pm
@@ -9,7 +9,7 @@ use vars qw($VERSION);
 
 $VERSION = '0.03';
 
-use constant DEBUG => 0;
+use constant DEBUG => 1;
 use constant TRACE => 0;
 use constant ASSERT => 1;
 use constant STEPPING => 1;
@@ -209,6 +209,12 @@ sub add_check_block {
     print "New check block $node: " . ($bone->pp) . "\n";
   }
 
+  # Make reciprocal links
+  my ($min, $max) = $bone->unknowns_range;
+  for ($min .. $max) {
+    $self->{bottom}->[$bone->[$_]]->{$node} = $bone;
+  }
+
   # mark node as pending resolution
   push @{$self->{unresolved}}, $node;
 
@@ -234,15 +240,12 @@ sub apply_solution {
 # Work up from a newly-solved block, potentially doing up-propagation
 # rule
 sub cascade {
-  my ($self,$node,$newly_solved) = @_;
+  my ($self,$node) = @_;
 
-  my $mblocks = $self->{mblocks};
-  my $ablocks = $self->{ablocks};
+  my $mblocks  = $self->{mblocks};
+  my $ablocks  = $self->{ablocks};
   my $coblocks = $mblocks + $ablocks;
-  my $pending = $self->{unresolved};
-
-  die "Cascade needs a solved array"
-    if ASSERT and ref($newly_solved) ne 'ARRAY';
+  my $pending  = $self->{unresolved};
 
   my @upper = keys %{$self->{bottom}->[$node]};
 
@@ -258,31 +261,11 @@ sub cascade {
   # update the count of unsolved edges and maybe solve aux blocks
   for my $to (@upper) {
     print "Decrementing unknowns count for block $to\n" if DEBUG;
-    my $old_count = ($self->{unknowns}->[$to - $mblocks])--;
-
-    # apply upward propagation rule on unsolved aux block?
-    if ($old_count == 1 and $to < $coblocks 
-	and !$self->{solution}->[$to]) {
-
-      my $bone = $self->{top}->[$to - $mblocks];
-      $self->{$node}->[$to - $mblocks] = undef;
-
-      die "Aux node didn't have $node as an unsolved\n"
-	if ($to != $bone->one_unknown($to));
-      print "Marking block $to as solved\n" if DEBUG;
-      $self->{solution}->[$to] = $bone;
-
-      my ($min, $max) = $bone->knowns_range;
-      for ($min .. $max) {
-	delete $self->{bottom}->[$_]->{$to};
-      }
-	  
-      push @$newly_solved, $bone;
-
-    } else {
-      push @$pending, $to;
-    }
+    ($self->{unknowns}->[$to - $mblocks])--;
+    
   }
+  push @$pending, @upper;
+  
 }
 
 sub resolve {
@@ -315,98 +298,113 @@ sub resolve {
 
   while (@$pending) {
 
-    my ($from, $toindex, $to) = (shift @$pending);
+    my ($from, $to, $min, $max) = (shift @$pending);
 
-    my @solved_nodes = ();
-    my @unsolved_nodes;
-    my $bone =  $self->{top}->[$from - $mblocks];
+    my $bone     = $self->{top}     ->[$from - $mblocks];
     my $unknowns = $self->{unknowns}->[$from - $mblocks];
+    my $solved   = $self->{solution}->[$from];
 
     if (DEBUG) {
       print "\nStarting resolve at $from: " . $bone->pp .
 	" ($unknowns unknowns)\n";
     }
-
-    next unless $unknowns == 1;
-
-    # Propagation rule matched (one unknown down edge)
-
-    # resolve() only solves a node if the upper node itself is solved.
-    # cascade() will handle the case of solving an unsolved aux block
-    # by solving its last unsolved message block (upward propagation)
-
-    my $solved = $self->{solution}->[$from];
-
-    my @upper    = ();
-
     if (DEBUG) {
       my ($type, $status) = ("check", "unsolved");
       $type   = "auxiliary" if $from < $coblocks;
       $status = "solved"    if $solved;
-
-      # We don't know the 'to' node yet; just print what we do know
       print "Propagating from $status $type node $from\n";
     }
 
-    # pull out the unknown node
-    if ($solved) {
-      $to = $bone->one_unknown($self);
-    }
+    # I'm going back to the old way of doing up-propagation since the
+    # new way messes with single-stepping
 
-    if ($from < $coblocks and !$solved) {
-      print "Skipping down propagation rule on unsolved aux\n" if DEBUG;
-      next;
+    if ($unknowns == 0) {
 
-    } elsif (DEBUG) {
+      next unless $from < $coblocks and !$solved;
 
-      my ($type, $status) = ("auxiliary", "an unsolved");
-      $type   = "message" if $to < $mblocks;
-      $status = "a solved"  if $self->{solution}->[$to];
+      if (DEBUG) {
+	print "Solving aux block $from based on aux rule\n";
+      }
 
-      # Now we know the 'to' node:
-      print "To node $to is $status $type node.\n";
-    }
+      die "Aux rule: didn't have one unknown\n"
+	if ($from != $bone->one_unknown($self));
 
-    # delete reciprocal links for all known edges
-    my ($min, $max) = $bone->knowns_range;
-    for ($min .. $max) {
-      delete $self->{bottom}->[$bone->[$_]]->{$from};
-    }
+      $self->{solution}->[$from] = $bone;
 
-    # mark child node as solved
-    print "Marking block $to as solved\n" if DEBUG;
-    $self->{solution}->[$to] = $bone;
-    push @newly_solved, $bone;
+      # delete all edges that point up to us
+      ($min,$max) = $bone->knowns_range;
+      for ($min .. $max) {
+	my $message = $bone->[$_];
+	die "Tried to delete non-existent up edge\n" if ASSERT and
+	  !exists($self->{bottom}->[$message]->{$from});
+	delete $self->{bottom}->[$message]->{$from};
+      }
 
-    # Remember which nodes to cascade to
-    @upper = keys %{$self->{bottom}->[$to]};
+      $self->{top}->[$from - $mblocks] = undef;
+      
+      push @newly_solved, $bone;
+      push @$pending, keys %{$self->{bottom}->[$from]};
 
-    if (DEBUG) {
-      if (@upper > 1) {
-	print "Solved node $to cascades to nodes " . 
-	  (join " ", @upper) . "\n";
+    } elsif ($unknowns == 1) {
+
+      # Propagation rule matched (one unknown down edge)
+
+      # resolve() only solves a node if the upper node itself is solved.
+      # cascade() will handle the case of solving an unsolved aux block
+      # by solving its last unsolved message block (upward propagation)
+
+
+      if ($from < $coblocks and !$solved) {
+	print "Skipping down propagation rule on unsolved aux\n" if DEBUG;
+	next;
+      }
+
+      # pull out the unknown node
+      if ($solved) {
+	$to = $bone->one_unknown($self);
+      }
+
+      if (DEBUG) {
+	my ($type, $status) = ("auxiliary", "an unsolved");
+	$type   = "message" if $to < $mblocks;
+	$status = "a solved"  if $self->{solution}->[$to];
+	print "To node $to is $status $type node.\n";
+      }
+
+      # delete reciprocal links for all known edges
+      ($min, $max) = $bone->knowns_range;
+      for ($min .. $max) {
+	my $lower = $bone->[$_];
+	# next if $lower == $from;
+	# die "Tried to delete non-existent up edge $lower->$from\n" 
+	#   if ASSERT and !exists($self->{bottom}->[$lower]->{$from});
+	delete $self->{bottom}->[$lower]->{$from};
+      }
+
+      # mark child node as solved
+      print "Marking block $to as solved\n" if DEBUG;
+      $self->{solution}->[$to] = $bone;
+      push @newly_solved, $bone;
+
+      if ($to < $mblocks) {
+	print "Solved message block $to completely\n" if DEBUG;
+	unless (--($self->{unsolved_count})) {
+	  $self->{done} = 1;
+	  # comment out next two lines to continue decoding just in
+	  # case there's a bug later
+	  @$pending = ();
+	  last;                 # finish searching
+	}
       } else {
-	print "Solved node $to has no cascade\n";
+	print "Solved auxiliary block $to completely\n" if DEBUG;
+	push @$pending, $to;
       }
+      cascade($self, $to, \@newly_solved);
     }
-
-    if ($to < $mblocks) {
-      print "Solved message block $to completely\n" if DEBUG;
-      unless (--($self->{unsolved_count})) {
-	$self->{done} = 1;
-	# comment out next two lines to continue decoding just in
-	# case there's a bug later
-	@$pending = ();
-	last;                 # finish searching
-      }
-    } else {
-      print "Solved auxiliary block $to completely\n" if DEBUG;
-      push @$pending, $to;
-    }
-    cascade($self, $to, \@newly_solved);
 
     return ($self->{done}, @newly_solved)
-      if (@newly_solved and STEPPING)
+      if (@newly_solved and STEPPING);
+
   }
 
   return ($self->{done}, @newly_solved);
