023b58fc219d5e91101b717ad0af5d3474257211
Declan Malone <idablack@users.sourceforge.net>
2019-11-18 22:03:46 +0000

    Working with WS echo example in Guides::Cookbook as a base.
    I want to have nodes in a cluster respond to two types of message:
    
    * split and send some number of shares to other node(s)
    * receive share from some node and store it
    
    I want to use regular sockets for the share transport, so the
    first step is to write the receiving side. This script will
    take a file name/transaction ID/whatever (it doesn't matter which)
    and set up a new listening port, then report back to the ws client
    on the port number.
    
    Later on, to simulate receiving a share, when the client receives
    a file over the port, it calculates a SHA sum of the data and
    reports it back on the ws channel (with the same filename/trans-
    action id/etc.).

---
+++ mojo-experiments/net-sha.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+use Digest::SHA;
+
+# 
+
+get '/' => sub {
+  my $c = shift;
+  $c->render(template => 'index');
+};
+
+# WebSocket service 
+websocket '/sha' => sub {
+    my $c = shift;
+
+    # Opened
+    $c->app->log->debug('WebSocket opened');
+
+    # Increase inactivity timeout for connection a bit
+    $c->inactivity_timeout(300);
+
+    # Incoming message
+    $c->on(message => sub {
+	my ($c, $msg) = @_;
+	$c->send("echo: $msg");
+	$c->app->log->debug("Got message $msg\n");
+           });
+
+    # Closed
+    $c->on(finish => sub {
+	my ($c, $code, $reason) = @_;
+	$c->app->log->debug("WebSocket closed with status $code");
+           });
+};
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'Welcome';
+<h1>Mojolicious WebSocket Example</h1>
+
+Enter a filename below to open a new port to receive that file on. You
+can use netcat to send the file to that port and we will receive a
+message back here that reports on the SHA1 hash.
+
+<!-- <input id="form"> <input id="submit" type="submit"> -->
+<form id="form">
+  <div id="sub-topic-field" class="form-group">
+    <label for="sub-topic">Filename: </label>
+    <div class="input-group">
+      <input type="text" id="sub-topic" class="form-control" />
+      <span class="input-group-btn">
+         <button class="btn btn-primary">Send</button>
+      </span>
+    </div>
+  </div>
+</form>
+
+<h3>Transcript</h3>
+
+<pre id="preblock">
+</pre>
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%= title %></title></head>
+  <body>
+  <%= content %>
+
+  <!-- if I put the script first, getElementById fails -->
+  <script>
+    var pre = document.getElementById("preblock");
+    var ws  = new WebSocket('<%= url_for('sha')->to_abs %>');
+    // var form = document.getElementById("form");
+
+    document.forms["form"].onsubmit 
+      = function(e) { e.preventDefault(); ws.send(e); };
+
+    // Incoming messages
+    ws.onmessage = function (event) {
+	pre.innerHTML += event.data + "\n";
+	// document.body.innerHTML += event.data + '<br/>';
+    };
+
+    // Outgoing messages
+    ws.onopen = function (event) {
+      window.setInterval(function () { 
+	ws.send('Hello Mojo!')
+      }, 1000);
+    };
+
+  </script>
+  </body>
+</html>
