a4fbb13941828e6af9b0bdf2860d8a251a03bfa2
Declan Malone <idablack@users.sourceforge.net>
2019-11-19 20:52:28 +0000

    Working on sender side. Not connecting at the moment.

--- mojo-experiments/net-sha.pl
+++ mojo-experiments/net-sha.pl
@@ -1,6 +1,110 @@
 #!/usr/bin/env perl
+
+package Mojolicious::Command::sendfile;
+use Mojo::Base 'Mojolicious::Command';
+
+# Short description
+has description => 'My first Mojo command';
+
+# Usage message from SYNOPSIS
+has usage => sub { shift->extract_usage };
+
+use v5.20;
+use Mojo::UserAgent;
+use Mojo::Promise;
+
+# We have to jump through hoops to read a single message from the
+# websocket...
+sub get_response {
+    my $tx = shift;
+    my $promise = Mojo::Promise->new;
+    $tx->once(message => sub {
+	my ($tx,$msg) = @_;
+	$promise->resolve($msg);
+	      });
+    return $promise;
+}
+
+sub run {
+    my ($self, $host, $port, $file, @junk) = @_;
+
+    if ($host ne "localhost") {
+	die "We only support connecting to localhost for now\n";
+    }
+
+    my $ua = Mojo::UserAgent->new;
+    $ua->websocket("ws://$host:$port/sha" => sub {
+	my ($ua, $tx) = @_;
+	say 'WebSocket handshake failed!' and return unless $tx->is_websocket;
+	$tx->on(finish => sub {
+	    my ($tx, $code, $reason) = @_;
+	    say "WebSocket closed with status $code.";
+		});
+
+	# Use get_response above to wait for a single response message
+	$tx->send("RECEIVE $file");
+	my $send_port;
+	get_response($tx)->then(sub {
+	    my $msg = shift;
+	    if ($msg =~ /port (\d+)/i) {
+		$send_port = $1;
+		warn "Will send to port $send_port\n";
+	    } else {
+		die "Message from server '$msg' didn't list port number\n";
+	    }
+	    $tx->on(message => sub {
+		my ($tx,$msg) = @_;
+		warn "$msg\n";
+		    });
+	    $tx->send("SEND $send_port $file");
+				});
+
+		   });
+
+    Mojo::IOLoop->start;
+    
+}
+
+=head1 SYNOPSIS
+
+Usage: APPLICATION sendfile localhost port ./filename
+
+=head1 DESCRIPTION
+
+Exercises the sender and receiver sides of the demo:
+
+=over
+
+=item 1. Makes a WebSocket connection to a running instance of this app on localhost:port
+
+=item 2. Sends a RECEIVE command with the file name and gets back a port number
+
+=item 3. Sends a SEND command with the file name and the same port number
+
+=item 4. Sender and receiver transfer the file
+
+=item 5. Receiver calculates SHA1 hash and reports it back to us
+
+=back
+
+=head1 NOTES
+
+The sender side of the demo is restricted to only sending files in the
+same directory as the script itself. This is just a very basic
+security feature to prevent exfiltration of sensitive files
+from the system.
+
+=cut
+
+    
+1;
+
+package main;
+
 use Mojolicious::Lite;
 use Digest::SHA;
+
+use FindBin qw($Bin);
 
 use Mojo::IOLoop::Server;
 
@@ -24,39 +128,87 @@ websocket '/sha' => sub {
 	my ($c, $msg) = @_;
 	$c->app->log->debug("Got message $msg\n");
 
-	if (exists(app->{transactions}->{$msg})) {
-	    my $port = app->{transactions}->{$msg}->{port};
-	    $c->send("$msg: already running on port $port");
-	    return;
-	}
-
-	my $server = Mojo::IOLoop::Server->new;
-	$server->on(accept => sub {
-	    my ($server,$handle) = @_;
-	    $c->app->log->debug("accepted connection\n");
-	    $server->stop;	# only accept one connection
-	    my $sum = Digest::SHA->new("sha1");
-	    my $stream = Mojo::IOLoop::Stream->new($handle);
-	    $stream->on(read => sub {
-		my ($stream,$data) = @_;
-		$sum->add($data);
-			});
-	    $stream->on(close => sub {
-		$c->app->log->debug("closing connection\n");
-		my $hex = $sum->hexdigest;
-		$c->send("$msg: $hex");
-		delete app->{transactions}->{$msg};
-			});
-	    app->{transactions}->{$msg}->{stream}=$stream;
-	    $stream->start;
-		    });
-	$server->listen(port => 0);
-	my $port = $server->port;
-	app->{transactions}->{$msg}->{port}=$port;
-	app->{transactions}->{$msg}->{server}=$server;
-	$server->start;
+	# Receiver side
+	if ($msg =~ /^RECEIVE (.*)$/) {
+	    $msg = $1;
 	
-	$c->send("Port $port ready to receive $msg");
+	    if (exists(app->{transactions}->{$msg})) {
+		my $port = app->{transactions}->{$msg}->{port};
+		$c->send("$msg: already running on port $port");
+		return;
+	    }
+
+	    my $server = Mojo::IOLoop::Server->new;
+	    $server->on(accept => sub {
+		my ($server,$handle) = @_;
+		$c->app->log->debug("accepted connection");
+		$server->stop;	# only accept one connection
+		my $sum = Digest::SHA->new("sha1");
+		my $stream = Mojo::IOLoop::Stream->new($handle);
+		$stream->on(read => sub {
+		    my ($stream,$data) = @_;
+		    my $size = length($data);
+		    $c->app->log->debug("read $size bytes");
+		    $sum->add($data);
+			    });
+		$stream->on(close => sub {
+		    $c->app->log->debug("closing connection");
+		    my $hex = $sum->hexdigest;
+		    $c->send("$msg: $hex");
+		    delete app->{transactions}->{$msg};
+			    });
+		app->{transactions}->{$msg}->{stream}=$stream;
+		$stream->start;
+			});
+	    $server->listen(port => 0);
+	    my $port = $server->port;
+	    app->{transactions}->{$msg}->{port}=$port;
+	    app->{transactions}->{$msg}->{server}=$server;
+	    $server->start;
+	    
+	    $c->send("Port $port ready to receive $msg");
+
+        # Sender side
+	} elsif ($msg =~ /^SEND (\d+) (.*)$/) {
+
+	    my ($port, $file) = ($1,$2);
+	    # Only allow sending of file in current directory
+	    if ($file =~ m|^\./[^/]+$|) {
+		if (!open my $fh, "<", "$Bin/$file") {
+		    $c->send("No such file '$file'");
+		} else {
+		    # IOLoop::Stream can read from a file, but not
+		    # write to one!
+		    my $client = Mojo::IOLoop::Client->new;
+		    $client->on(connect => sub {
+			my ($client, $handle) = @_;
+			$c->app->log->debug("Sender connected to receiver");
+			my $istream = Mojo::IOLoop::Stream->new($fh);
+			my $ostream = Mojo::IOLoop::Stream->new($handle);
+			$ostream->start;
+			$istream->on(read => sub {
+			    my ($istream,$data) = @_;
+			    my $size = length($data);
+			    $c->app->log->debug("sent $size bytes");
+			    $ostream->write($data);
+				     });
+			$istream->start;
+				});
+		    $client->on(error => sub {
+			my ($client, $err) = @_;
+			$c->send("Sender: Error connecting: $err");
+				});
+		    $client->connect(address => 'localhost',
+				     port => $port);
+		    $client->reactor->start unless $client->reactor->is_running;
+		}
+	    } else {
+		$c->send("Invalid filename '$file'");
+	    }
+
+	} else {
+	    $c->send("Invalid command!");
+	}	    
            });
 
     # Closed
@@ -67,6 +219,8 @@ websocket '/sha' => sub {
 };
 
 app->start;
+
+1;
 __DATA__
 
 @@ index.html.ep
@@ -117,7 +271,7 @@ and we get back the SHA1 of that file over the WebSocket.</p>
         e.preventDefault();
         // is there no better way of doing this based on ID?
         var val = document.forms["form"].elements.item(0).value;
-        ws.send(val);
+        ws.send("RECEIVE " + val);
       };
 
     // Incoming messages
