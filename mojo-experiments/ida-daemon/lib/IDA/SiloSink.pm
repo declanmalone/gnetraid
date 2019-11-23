248a8955d328c2a2c553ce6205b7b93d60378ccf
Declan Malone <idablack@users.sourceforge.net>
2019-11-23 16:52:53 +0000

    Add subscriptions to source emitter. Add some documentation.

--- mojo-experiments/ida-daemon/lib/IDA/SiloSink.pm
+++ mojo-experiments/ida-daemon/lib/IDA/SiloSink.pm
@@ -36,10 +36,10 @@ sub config {
 # Arguments:
 #
 # $source    another EventEmitter that emits events
-# $read      the name of the "read"-like event that signals us that
-#            more data is available to read/process
-# $close     the name of the "close"-like event that signals us that
-#            no more data will be arriving (ie, eof)
+# $read      the name of the "read"-like event from $source that signals
+#            us that more data is available to read/process
+# $close     the name of the "close"-like event from $source that signals
+#            us that no more data will be arriving (ie, eof)
 #
 # Although we don't do any special processing here on the on-close
 # event, other classes that follow the same design pattern (eg, a sink
@@ -114,14 +114,99 @@ sub new {
 	unless ($source->isa("Mojo::EventEmitter"));
 
     # The calling program should generally set up the full processing
-    # chain before starting any Source objects (generally a
+    # chain before starting any Source objects (such as a
     # Mojo::IOLoop::Stream object) to feed the chain. Included in that
     # is stashing the constructed objects and subscribing to their
     # error events. Thus by the time any callbacks below get
     # triggered, we should be able to raise error events that can be
     # handled by the caller.
 
+    $source->on($read => sub {
+	my ($s, $data) = @_;
+	if (!print $fh $data) {
+	    # assume this is fatal, so close handle and unsubscribe
+	    close $fh;
+	    $source->unsubscribe($read);
+	    $source->unsubscribe($close) if defined($close);
+	    # not sure if print sets $!... anyway, emit error
+	    $self->emit(error => "Failed to write: $!");
+	}}
+    );
+    return $self unless defined($close);
+
+    # We can propagate close events along a chain so that
+    # the caller only has to subscribe to the close event of
+    # the last emitter in the chain.
+    $source->on($close => sub {
+	my $s = shift;
+	close $fh;
+	$source->unsubscribe($read);
+	$source->unsubscribe($close);
+	$self->emit("close");
+		}
+    );
+
     $self;
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+ use IDA::SiloSink;
+ use Mojo::IOLoop::Stream;
+
+ # configure class to allow creation of files under ./data
+ IDA::SiloSink->config("data"); 
+
+ # create a source stream that emits "read" events and, optionally,
+ # "close" events
+ open my $fh, "<", "./data/foo" or die;
+ my $stream = Mojo::IOLoop::Stream->new($fh);
+
+ # create a sink that subscribes to the source stream, writing the
+ # data stream to ./data/bar
+ my $o = IDA::SiloSink->new($stream, "read", "close", "data/bar"); 
+
+ # must check whether new() succeeded ...
+ die "$o\n" unless ref($o);  # NB: returns error message, not undef
+
+ # ... before trying to call methods on $o
+ $o->on("close" => sub { warn "Chain completed without error"}) ;
+ $o->on("error" => sub { die  "$_[1]" });
+
+ $stream->start;         # make source stream active
+ Mojo::IOLoop->start;    # start event loop
+
+=head1 DESCRIPTION
+
+This is one of a set of stream processing primitives (sources, filters
+and sinks) intended to make it easier to set up asynchronous stream
+processing pipelines.
+
+All classes derive from Mojo::Emitter, and may emit the following events:
+
+=over
+
+=item read
+
+Indicates that some new data is available for a downstream processing element
+
+=item error
+
+Indicates some unrecoverable error
+
+=item close
+
+Indicates the end of the stream
+
+=back
+
+These event names match up with those used in Mojo::IOLoop::Stream,
+which is itself a Mojo::EventEmitter, so can be used freely as part of
+a processing pipeline.
+
